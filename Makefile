23dcab0554b72e344eef2afcae87517c2954cfea
Christophe Mehay <cmehay@student.42.fr>
2013-11-22 15:38:17 +0100

    take that moulinette

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2013/11/19 15:09:23 by cmehay            #+#    #+#              #
-#    Updated: 2013/11/22 11:26:05 by cmehay           ###   ########.fr        #
+#    Updated: 2013/11/22 14:53:15 by cmehay           ###   ########.fr        #
 #                                                                              #
 #******************************************************************************#
 
@@ -20,7 +20,11 @@ SRC = ft_memset.c ft_putchar.c ft_putstr.c ft_bzero.c ft_putchar_fd.c \
 	ft_strlen.c ft_strcat.c ft_strncat.c ft_strtrim.c ft_isalpha.c \
 	ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_atoi.c ft_tolower.c \
 	ft_toupper.c ft_memalloc.c ft_strnew.c ft_memcpy.c ft_memccpy.c \
-	ft_memmove.c ft_memchr.c
+	ft_memmove.c ft_memchr.c ft_memcmp.c ft_strdup.c ft_strchr.c ft_strlcat.c \
+	ft_strrchr.c ft_strstr.c ft_strnstr.c ft_strcmp.c ft_strncmp.c ft_memdel.c \
+	ft_strdel.c ft_strclr.c ft_striter.c ft_striteri.c ft_strmap.c \
+	ft_strmapi.c ft_strequ.c ft_strnequ.c ft_strsub.c ft_strjoin.c \
+	ft_strsplit.c
 SRCFILES = $(addprefix $(SRCDIR)/, $(SRC))
 OBJ = $(SRC:.c=.o)
 CC = gcc
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:05:29 by cmehay            #+#    #+#             */
-/*   Updated: 2013/11/22 11:27:44 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/11/22 15:32:05 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,33 @@ void	*ft_memcpy(void *s1, void *s2, size_t n);
 void	*ft_memccpy(void *s1, void *s2, int c, size_t n);
 void	*ft_memmove(void *s1, void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strdup(const char *s1);
+
+/*not yet done*/
+char	*ft_strcpy(char *s1, const char *s2);
+char	*ft_strncpy(char *s1, const char *s2, size_t n);
+int		ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *s1, const char *s2);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+void	ft_memdel(void **ap);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
+char	**ft_strsplit(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
