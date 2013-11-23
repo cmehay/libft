dec8f7488f2b60597dbc1ec77fcab405b0431ccd
Christophe Mehay <cmehay@student.42.fr>
2013-11-23 14:01:57 +0100

    ft_strstr.c ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,11 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/11/22 15:05:26 by cmehay            #+#    #+#             */
-/*   Updated: 2013/11/22 15:06:02 by cmehay           ###   ########.fr       */
+/*   Created: 2013/11/22 14:59:37 by cmehay            #+#    #+#             */
+/*   Updated: 2013/11/23 13:56:19 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char    *ft_strnstr(const char *s1, const char *s2, size_t n)
+{
+	size_t	s2_len;
+	size_t	i;
 
+	if (s2_len == 0)
+		return ((char*) s1);
+	i = 0;
+	s2_len = ft_strlen(s2);
+	while (*s1 != 0 && i < n)
+	{
+		if (ft_memcmp(s1, s2, s2_len) == 0)
+			return ((char*) s1);
+		s1++;
+		i++;
+	}
+	return (NULL);
+}
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 14:59:37 by cmehay            #+#    #+#             */
-/*   Updated: 2013/11/23 13:26:02 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/11/23 13:46:01 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char    *ft_strstr(const char *s1, const char *s2)
 	size_t	s2_len;
 
 	s2_len = ft_strlen(s2);
+	if (s2_len == 0)
+		return ((char*) s1);
 	while (*s1 != 0)
 	{
 		if (ft_memcmp(s1, s2, s2_len) == 0)
