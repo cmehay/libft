98b944c699056b6f084574ceb0db9f6c054fbe8f
Christophe Mehay <cmehay@student.42.fr>
2013-11-26 11:10:13 +0100

    ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 15:28:25 by cmehay            #+#    #+#             */
-/*   Updated: 2013/11/25 18:04:10 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/11/26 11:02:09 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,19 @@ static size_t	word_counter(char const *s, char c)
 	return (cnt);
 }
 
-static int		add_str(char *to, char const *from, char c)
+static int		add_str(char **to, char const *from, char c, int index)
 {
 	size_t	i;
 	size_t	rtn;
 
 	i = 0;
-	while (from[i] == c || from[i] == 0)
+	while (from[i] != c && from[i] != 0)
 		i++;
-	if ((to = ft_strnew(i++)) == NULL)
+	if ((to[index] = ft_strnew(i)) == NULL)
 		return (0);
 	rtn = i;
 	while (i-- > 0)
-		to[i] = from[i];
+		to[index][i] = from[i];
 	return (rtn);
 }
 
@@ -51,19 +51,21 @@ char	**ft_strsplit(char const *s, char c)
 	char	**rtn;
 	char	*tmp;
 	size_t	i;
+	int		j;
 
 	if ((rtn = (char**) malloc(word_counter(s, c) * sizeof(char*))) == NULL)
 		return (NULL);
 	tmp = (char*) s;
 	i = 1;
+	j = 0;
 	if (*tmp != c)
-		if ((i = add_str(*rtn++, tmp, c)) == 0)
+		if ((i = add_str(rtn, tmp, c, j++)) == 0)
 			return (NULL);
 	while (tmp[i] != 0)
 	{
 		if ((tmp[i] != c && tmp[i - 1] == c))
 		{
-			if ((i += add_str(*rtn++, tmp + i, c)) == 0)
+			if ((i += add_str(rtn, tmp + i, c, j++)) == 0)
 				return (NULL);
 		}
 		else
