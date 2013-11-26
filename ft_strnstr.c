99c9f36a4bdd17da27e3244d5ed624e42b1322e6
Christophe Mehay <cmehay@student.42.fr>
2013-11-26 12:27:30 +0100

    correction ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 14:59:37 by cmehay            #+#    #+#             */
-/*   Updated: 2013/11/24 12:55:42 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/11/26 12:25:19 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char    *ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	s2_len;
 	size_t	i;
 
-	if (s2_len == 0 || n == 0 || *s2 == 0)
+	s2_len = ft_strlen(s2);
+	if (s2_len == 0 || n == 0)
 		return ((char*) s1);
 	i = 0;
-	s2_len = ft_strlen(s2);
 	while (*s1 != 0 && i < n && (i + s2_len <= n))
 	{
 		if (ft_memcmp(s1, s2, s2_len) == 0)
