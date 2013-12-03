b7a5e1912bef79c90f4613059073d315a7f46c06
Christophe Mehay <cmehay@student.42.fr>
2013-12-03 16:52:41 +0100

    ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 10:10:23 by cmehay            #+#    #+#             */
-/*   Updated: 2013/11/24 15:12:24 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/12/02 15:08:38 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,29 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	{
-		unsigned int	usgn_copy;
-		int				nb_size;
-		int				nb_copy;
-		int				nb_print;
+	unsigned int	usgn_copy;
+	int				nb_size;
+	int				nb_copy;
+	int				nb_print;
 
-		if (n < 0)
-		{
-			ft_putchar_fd('-', fd);
-		}
-		usgn_copy = n * ((n > 0) + (n > 0) - 1);
-		nb_size = 1;
-		nb_copy = usgn_copy;
-		while (nb_copy / 10 > 0)
-		{
-			nb_size *= 10;
-			nb_copy /= 10;
-		}
-		while (nb_size > 0)
-		{
-			nb_print = 0;
-			nb_print = usgn_copy / nb_size;
-			ft_putchar_fd(nb_print + '0', fd);
-			usgn_copy = usgn_copy % nb_size;
-			nb_size = nb_size / 10;
-		}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+	}
+	usgn_copy = n * ((n > 0) + (n > 0) - 1);
+	nb_size = 1;
+	nb_copy = usgn_copy;
+	while (nb_copy / 10 > 0)
+	{
+		nb_size *= 10;
+		nb_copy /= 10;
+	}
+	while (nb_size > 0)
+	{
+		nb_print = 0;
+		nb_print = usgn_copy / nb_size;
+		ft_putchar_fd(nb_print + '0', fd);
+		usgn_copy = usgn_copy % nb_size;
+		nb_size = nb_size / 10;
 	}
 }
